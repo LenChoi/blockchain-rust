56078bd1c847e7d2b057da87281332aab437f0e8
Len <len.choi@cyworldlabs.com>
2021-11-10 20:57:21 +0900

    블록, 해쉬 추가

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main () {
-    println!("Hello, blockchain!");
+use blockchainlib::{Block, Hashable};
+
+fn main() {
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+
+    println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
+
+    block.hash = h;
+
+    println!("{:?}", &block);
 }

9791223f5fae26e99223189694cb6529d6c8fad7
Len <len.choi@cyworldlabs.com>
2021-11-14 20:26:19 +0900

    add verify blocks

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-use blockchainlib::{Block, Hashable};
+use blockchainlib::*;
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, now(), vec![0; 32], 0,
+                               "Genesis block".to_owned(), difficulty);
 
-    println!("{:?}", &block);
+    block.mine();
 
-    let h = block.hash();
+    println!("Mined genesis block {:?}", &block);
 
-    println!("{:?}", &h);
+    let mut last_hash = block.hash.clone();
 
-    block.hash = h;
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
 
-    println!("{:?}", &block);
+    println!("Verify: {}", &blockchain.verify());
+
+    for i in 1..10 {
+        let mut block = Block::new(i, now(), last_hash, 0,
+                                   "Another block".to_owned(), difficulty);
+        block.mine();
+        println!("Mined block {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+
+        println!("Verify: {}", &blockchain.verify());
+    }
+
+    blockchain.blocks[3].payload = "Nope".to_owned();
+    println!("Verify: {}", &blockchain.verify());
 }
